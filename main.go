53b7435e93f543f05ab34305693ec16a3196d713
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-04-28 23:53:14 +0200

    add tests

--- main.go
+++ main.go
@@ -4,16 +4,56 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
-func main() {
-	in, err := ioutil.ReadFile("data/foo.csv")
+type transaction struct {
+	date  string
+	iban  string
+	value float32
+}
+
+func loadFile(filePath string) string {
+	in, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	raw = string(in)
+	return string(in)
+}
 
-	fmt.Println(raw)
+func getIBAN(content string) string {
+	return "TODO"
+}
+
+func parseCSV(content string) []transaction {
+	startOffset := 17
+	endOffset := 4
+	start := strings.Index(content, "Umsatz in EUR")
+	end := strings.Index(content, "Alter Kontostand")
+	rawEntries := content[start+startOffset : end-endOffset]
+	entries := strings.Split(rawEntries, "\n")
+
+	for i := 0; i < len(entries); i++ {
+		entry := entries[i]
+		entryParts := strings.Split(entry, ";")
+
+		date := entryParts[0]
+		iban := getIBAN(entryParts[3])
+		amount := entryParts[4]
+		fmt.Println("\n{")
+		fmt.Println(date)
+		fmt.Println(iban)
+		fmt.Println(amount)
+		fmt.Println("}\n")
+	}
+
+	return nil
+}
+
+func main() {
+	raw := loadFile("data/foo.csv")
+	parseCSV(raw)
+	// fmt.Println(raw)
 
 }
---
+++ main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestGetIBAN(t *testing.T) {
+	var ibanTests = []struct {
+		in  string // input
+		out string // expected result
+	}{
+		{`Empf�nger: Daniel Schmidt, Carina Heitmann Kto/IBAN: DE60200411550892963000 BLZ/BIC: COBADEHD055 Buchungstext: End-to-End-Ref.: nicht angegeben Ref. JF217118L1327255/2";`, "DE60200411550892963000"},
+		{`Empf�nger: Landeshauptstadt Kiel Kto/IBAN: DE76210501701001803152 BLZ/BIC: NOLADE21KIE Buchungstext: 1057005241701 End-to-End-Ref.: nicht angegeben Ref. JF217118L1327288/2;`, "DE76210501701001803152"},
+	}
+
+	for _, tt := range ibanTests {
+		actual := getIBAN(tt.in)
+		if actual != tt.out {
+			t.Errorf("Fib(%s): expected %s, actual %s", tt.in, tt.out, actual)
+		}
+	}
+}
+
+func TestParseCSV(t *testing.T) {
+	assertet := []transaction{
+		{"02.05.2017", "DE60200411550892963000", -100.00},
+		{"12.05.2017", "DE76210501701001803152", -48.50},
+	}
+	demoCSV := loadFile("fixtures/demo1.csv")
+	result := parseCSV(demoCSV)
+
+	if len(result) != 2 {
+		t.Error("Not enough items found")
+	}
+
+	for i := 0; i < 2; i++ {
+		r := result[i]
+		a := assertet[i]
+		if r.date != a.date {
+			t.Error("Names are not the same")
+		}
+
+		if r.iban != a.iban {
+			t.Error("IBANs are not the same")
+		}
+
+		if r.value != a.value {
+			t.Error("values are not the same")
+		}
+	}
+}
